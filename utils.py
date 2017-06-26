d8eed1e4751abe3ceebc889b758114511b4c031b
Chad Upjohn <upjohnc@gmail.com>
2017-06-25 19:20:16 -0500

    accounting for data with less than 20 rows

--- utils.py
+++ utils.py
@@ -9,6 +9,8 @@ def get_nyse(stock_name):
 
 
 def get_clean_and_n(df_origin):
+    if df_origin.shape[0] < 20:
+        return None
     df = df_origin.copy()
     df = clean_nyse(df)
     df = calc_n(df)
@@ -16,6 +18,8 @@ def get_clean_and_n(df_origin):
 
 
 def calc_n(df_origin):
+    if df_origin is None or df_origin.shape[0] < 20:
+        return None
     # take in a dataframe
     # return a series of the N for that day
     df = df_origin.copy()
@@ -45,14 +49,9 @@ def calc_n(df_origin):
     return df
 
 
-def calc_buy_trigger(df_origin):
-    # take in a dataframe
-    # return the value that triggers a buy
-    df = df_origin.copy()
-    pass
-
-
 def clean_nyse(df_original):
+    if df_original is None or df_original.shape[0] < 20:
+        return None
     df = df_original.copy()
     df.columns = [i.lower() for i in df.columns]
     if df['high'].dtype == 'O' or df['low'].dtype == 'O':
