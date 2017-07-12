95f38d329cab886fa9f68ef582d9f0365f1a281a
Chad Upjohn <upjohnc@gmail.com>
2017-07-11 19:17:08 -0500

    adding start of backtest

---
+++ backtest_utils.py
@@ -0,0 +1,17 @@
+# use core dataframe as one dataset
+
+# create a positions table
+
+# if no open positions
+# look for a buy signal
+# then set exit, stop, and new buy
+
+# if open positions
+# look for exit or stop
+# then set sell for all open positions
+
+# look for new buy
+# then set new position, update the stop (2*n)
+
+# variable of the last row checked
+# loop while row < the last row in the dataframe
